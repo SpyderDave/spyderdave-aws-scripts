10e64a1c2fe0f8f8bd2d4deea1bcd9ec3d7f2da0
Dave Morrow <davemorrow@Daves-MacBook-Pro-2.local>
2020-09-10 16:02:53 -0400

    .

--- aws-organization-securityhub-enabler.sh
+++ aws-organization-securityhub-enabler.sh
@@ -1,3 +1,7 @@
+#
+# Assume Role
+# Get list of accounts
+# Write the list in CSV format to stdout (redirect as you see fit)
 ROLEARN=arn:aws:iam::948143805254:role/thcp/privilegedservice/AWSFoundationsBootstrappingRole
 temp_role=$(aws sts assume-role --role-arn ${ROLEARN} --role-session-name organizations-securityhub)
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
@@ -6,10 +10,12 @@ export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xarg
 echo "AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID
 echo "AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY
 echo "AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN
+
 ROOTID=$(aws organizations list-roots --query 'Roots[?Name.Value==Root].Id' --output text)
 echo "My RootId=$ROOTID"
 
 echo "Listing Children:"
-aws organizations list-children --parent-id r-zayt --child-type ORGANIZATIONAL_UNIT
+aws organizations list-children --parent-id ${ROOTID} --child-type ORGANIZATIONAL_UNIT
+
 echo "Listing Accounts"
 aws organizations list-accounts --output text --query 'Accounts[*].{ID:Id,Email:Email}' | sed -E 's/\s+/,/g'
