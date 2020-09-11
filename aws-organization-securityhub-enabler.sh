df5b9c38a4de091fa70a1146f840a4e865dcf506
Dave Morrow <davemorrow@Daves-MacBook-Pro-2.local>
2020-09-11 11:34:38 -0400

    more

--- aws-organization-securityhub-enabler.sh
+++ aws-organization-securityhub-enabler.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Assume Role
 # Get list of accounts
@@ -35,11 +36,14 @@ ROLEARN=arn:aws:iam::948143805254:role/thcp/privilegedservice/AWSFoundationsBoot
 if [[ -z $ORGUNIT ]]
 then 
   echo "Using Root OU"
-  ORGUNIT=Root 
+  ORGUNIT=Root
+  CHILDTYPE="ACCOUNT" 
 else 
   echo "Using OU: ${ORGUNIT}"
+  CHILDTYPE="ORGANIZATIONAL_UNIT"
 fi 
 
+echo "Assuming Role in the Master Account - to gain access to AWS Organizations"
 temp_role=$(aws sts assume-role --role-arn ${ROLEARN} --role-session-name organizations-securityhub)
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
@@ -48,11 +52,13 @@ echo "AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID
 echo "AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY
 echo "AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN
 
+echo "Obtaining the RootId"
 ROOTID=$(aws organizations list-roots --query 'Roots[?Name.Value==Root].Id' --output text)
 echo "My RootId=$ROOTID"
 
 echo "Listing Children:"
-aws organizations list-children --parent-id ${ROOTID} --child-type ORGANIZATIONAL_UNIT
+if [[ ]]
+aws organizations list-children --parent-id ${ORGUNIT} --child-type ${CHILDTYPE}
 
-echo "Listing Accounts"
-aws organizations list-accounts --output text --query 'Accounts[*].{ID:Id,Email:Email}' | sed -E 's/\s+/,/g'
+#echo "Listing Accounts"
+#aws organizations list-accounts --output text --query 'Accounts[*].{ID:Id,Email:Email}' | sed -E 's/\s+/,/g'
